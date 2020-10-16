d74e98f41a0d88112d8b47a49ad3e2a18b81731c
tbdoan <tbdoan@ucsd.edu>
2020-10-16 13:36:13 -0700

    added text in separate file

---
+++ src/pages/AboutPage/text.ts
@@ -0,0 +1,38 @@
+interface cardText {
+  readonly role: string;
+  readonly name: string;
+  readonly major: string;
+}
+
+export const text: cardText[] = [
+  {
+    role: 'President',
+    name: 'Stone Tao',
+    major: 'Computer Science and Cognitive Science (ML)',
+  },
+  {
+    role: 'Director of Operations',
+    name: 'Catherine Lee',
+    major: 'Computer Science',
+  },
+  {
+    role: 'Director of Marketing',
+    name: 'Jonathan Zamora',
+    major: 'Computer Science',
+  },
+  {
+    role: 'Event Lead',
+    name: 'Alvin Wang',
+    major: 'Computer Science',
+  },
+  {
+    role: 'Event Lead',
+    name: 'Edward Yang',
+    major: 'Computer Science',
+  },
+  {
+    role: 'Event Lead',
+    name: 'Jason Vega',
+    major: 'Computer Science',
+  },
+];
