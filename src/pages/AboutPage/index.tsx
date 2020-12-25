9d5d24ddb56c7a9c5b36902df05dfceaf549b605
Stone Tao <stonezt2019@gmail.com>
2020-12-24 16:53:34 -0800

    fixes

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,116 +1,188 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './index.less';
 import DefaultLayout from '../../components/layouts/default';
 import { Col, Row } from 'antd';
-import {Card } from '../../components/Card';
-import { directors,operations,dev,external,ex } from './text';
+import { Card } from '../../components/Card';
+import { directors, operations, dev, external, ex } from './text';
 
 const ColoredLine = () => (
-    <hr
-        style={{
-            color: "gray",
-            backgroundColor: "gray",
-            height: '10%'
-        }}
-    />
+  <hr
+    style={{
+      color: 'gray',
+      backgroundColor: 'gray',
+      height: '10%',
+    }}
+  />
 );
 
-function AboutPage() {
+const AboutPage = () => {
   return (
     <DefaultLayout>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </meta>
       <div className="AboutPage">
         <div>
           <h1>Hello, we are ACM AI at UCSD</h1>
-        </div >
-        <h2 >And these are the people running the show!</h2>
+        </div>
+        <h2>And these are the people running the show!</h2>
         <br />
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, { xs: 8, sm: 16, md: 24, lg: 32 }]} justify="center" >
+        <Row
+          gutter={[
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+          ]}
+          justify="center"
+        >
           {directors.map((cardText) => {
             return (
-              <Col >
-                <Card >
-                  <img src="https://i.stack.imgur.com/l60Hf.png" alt="Nope" width="40%" height="40%"></img>
+              <Col xs={12}>
+                <Card>
+                  <img
+                    src="https://i.stack.imgur.com/l60Hf.png"
+                    alt="Nope"
+                    width="40%"
+                    height="40%"
+                  ></img>
                   <h3>{cardText.role}</h3>
                   <p className="name">{cardText.name}</p>
-                  <p className="position">{cardText.year + " "}{cardText.major + " major"}</p>
+                  <p className="position">
+                    {cardText.year + ' '}
+                    {cardText.major + ' major'}
+                  </p>
                 </Card>
               </Col>
             );
           })}
         </Row>
         <ColoredLine />
-        <div >
+        <div>
           <h2> Our team of event leads who design and host all the events </h2>
         </div>
         <br />
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, { xs: 8, sm: 16, md: 24, lg: 32 }]} justify="center">
+        <Row
+          gutter={[
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+          ]}
+          justify="center"
+        >
           {operations.map((cardText) => {
             return (
-              <Col>
+              <Col xs={12}>
                 <Card>
-                  <img src="https://i.stack.imgur.com/l60Hf.png" alt="Nope" width="40%" height="40%"></img>
+                  <img
+                    src="https://i.stack.imgur.com/l60Hf.png"
+                    alt="Nope"
+                    width="40%"
+                    height="40%"
+                  ></img>
                   <h3>{cardText.role}</h3>
                   <p className="name">{cardText.name}</p>
-                  <p className="position">{cardText.year + " "}{cardText.major + " major"}</p>
+                  <p className="position">
+                    {cardText.year + ' '}
+                    {cardText.major + ' major'}
+                  </p>
                 </Card>
               </Col>
             );
           })}
         </Row>
         <ColoredLine />
-        <div >
-          <h2> Our team of developers working tirelessly to keep everything running smoothly </h2>
+        <div>
+          <h2>
+            {' '}
+            Our team of developers working tirelessly to keep everything running
+            smoothly{' '}
+          </h2>
         </div>
         <br />
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, { xs: 8, sm: 16, md: 24, lg: 32 }]} justify="center">
+        <Row
+          gutter={[
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+          ]}
+          justify="center"
+        >
           {dev.map((cardText) => {
             return (
-              <Col>
+              <Col xs={12}>
                 <Card>
-                  <img src="https://i.stack.imgur.com/l60Hf.png" alt="Nope" width="40%" height="40%"></img>
+                  <img
+                    src="https://i.stack.imgur.com/l60Hf.png"
+                    alt="Nope"
+                    width="40%"
+                    height="40%"
+                  ></img>
                   <h3>{cardText.role}</h3>
                   <p className="name">{cardText.name}</p>
-                  <p className="position">{cardText.year + " "}{cardText.major + " major"}</p>
+                  <p className="position">
+                    {cardText.year + ' '}
+                    {cardText.major + ' major'}
+                  </p>
                 </Card>
               </Col>
             );
           })}
         </Row>
         <ColoredLine />
-        <div >
+        <div>
           <h2> Our wonderful marketing and sponsorship team </h2>
         </div>
         <br />
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, { xs: 8, sm: 16, md: 24, lg: 32 }]} justify="center">
+        <Row
+          gutter={[
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+          ]}
+          justify="center"
+        >
           {external.map((cardText) => {
             return (
-              <Col>
+              <Col xs={12}>
                 <Card>
-                  <img src="https://i.stack.imgur.com/l60Hf.png" alt="Nope" width="40%" height="40%"></img>
+                  <img
+                    src="https://i.stack.imgur.com/l60Hf.png"
+                    alt="Nope"
+                    width="40%"
+                    height="40%"
+                  ></img>
                   <h3>{cardText.role}</h3>
                   <p className="name">{cardText.name}</p>
-                  <p className="position">{cardText.year + " "}{cardText.major + " major"}</p>
+                  <p className="position">
+                    {cardText.year + ' '}
+                    {cardText.major + ' major'}
+                  </p>
                 </Card>
               </Col>
             );
           })}
         </Row>
         <ColoredLine />
-        <div >
+        <div>
           <h2> Your hardwork will not be forgotten </h2>
         </div>
         <br />
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, { xs: 8, sm: 16, md: 24, lg: 32 }]} justify="center">
+        <Row
+          gutter={[
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+            { xs: 8, sm: 16, md: 24, lg: 32 },
+          ]}
+          justify="center"
+        >
           {ex.map((cardText) => {
             return (
               <Col>
                 <Card>
-                  <img src="https://i.stack.imgur.com/l60Hf.png" alt="Nope" width="40%" height="40%"></img>
+                  <img
+                    src="https://i.stack.imgur.com/l60Hf.png"
+                    alt="Nope"
+                    width="40%"
+                    height="40%"
+                  ></img>
                   <h3>{cardText.role}</h3>
                   <p className="name">{cardText.name}</p>
-                  <p className="position">{cardText.year + " "}{cardText.major + " major"}</p>
+                  <p className="position">
+                    {cardText.year + ' '}
+                    {cardText.major + ' major'}
+                  </p>
                 </Card>
               </Col>
             );
@@ -119,6 +191,6 @@ function AboutPage() {
       </div>
     </DefaultLayout>
   );
-}
+};
 
 export default AboutPage;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './index.less';
-import DefaultLayout from "../../components/layouts/default";
+import DefaultLayout from '../../components/layouts/default';
 import { Link } from 'react-router-dom';
 import DiscordLink from '../../components/DiscordLink';
 import { ACMEvent, fetchFutureEvents } from '../../actions/events';
@@ -15,38 +15,95 @@ function MainPage() {
   }, []);
   return (
     <DefaultLayout>
-      <div className='Main'>
-        <div className='hero'>
-          <h1 id='title'>ACM AI at UCSD</h1>
-          
-          <p className='subtext'>We aspire to inspire the next generation of AI advocates, engineers, and scientists.</p>
+      <div className="Main">
+        <div className="hero">
+          <h1 id="title">ACM AI at UCSD</h1>
+          <p className="subtext">
+            We aspire to inspire the next generation of AI advocates, engineers,
+            and scientists.
+          </p>
         </div>
         <div>
-          <div className='main-section'>
-            <h2 className='statement'>Model Your AI/ML Path</h2>
-            <p>Our goals are to help build a community of AI enthusiasts at UCSD and connect that community to the broader AI network. We also strive to keep AI fun and accessible to all. We want to help you navigate your path around the complex world of AI through workshops, competitions, networking events and more!</p>
-            <p>Don't know where to start? Scroll down or go to our <a href="/#events">events</a></p>
+          <div className="main-section">
+            <h2 className="statement">Model Your AI/ML Path</h2>
+            <p>
+              Our goals are to help build a community of AI enthusiasts at UCSD
+              and connect that community to the broader AI network. We also
+              strive to keep AI fun and accessible to all. We want to help you
+              navigate your path around the complex world of AI through
+              workshops, competitions, networking events and more!
+            </p>
+            <p>
+              Don't know where to start? Scroll down or go to our{' '}
+              <a href="/#events">events</a>
+            </p>
           </div>
-          <div className='main-section'>
-            <h2 className='statement' id='learn-ai'>Learn about AI and Get Involved!</h2>
+          <div className="main-section">
+            <h2 className="statement" id="learn-ai">
+              Learn about AI and Get Involved!
+            </h2>
             <h3>Workshops, Competitions, Events</h3>
-            <p>We host many, many, *many* events throughout each quarter at UCSD. Best way to be in the loop of new events, competitions etc. is to join our Discord at <DiscordLink />. We run workshops on introductory to advanced neural network concepts and programming to workshops on the Kaggle platform</p>
+            <p>
+              We host many, many, *many* events throughout each quarter at UCSD.
+              Best way to be in the loop of new events, competitions etc. is to
+              join our Discord at <DiscordLink />. We run workshops on
+              introductory to advanced neural network concepts and programming
+              to workshops on the Kaggle platform
+            </p>
             <h3>Networking, Research, Reading Groups</h3>
-            <p>If you're looking for more networking opportunities or research, we also run seminars in collaboration with AI professors at UCSD as well as host an AI reading group on our discord. Trust us, it is very interesting.</p>
-            <p>Make sure to also <Link to='/register'>register</Link> an account with us! That way we can reach out to you about new opportunities and you can enter our exciting competitions</p>
+            <p>
+              If you're looking for more networking opportunities or research,
+              we also run seminars in collaboration with AI professors at UCSD
+              as well as host an AI reading group on our discord. Trust us, it
+              is very interesting.
+            </p>
+            <p>
+              Make sure to also <Link to="/register">register</Link> an account
+              with us! That way we can reach out to you about new opportunities
+              and you can enter our exciting competitions
+            </p>
             <h3>Joining Us</h3>
-            <p>We are always looking for more people to join our mission and build the growing AI community at UCSD. The following roles are open to all undergraduate UCSD students:</p>
-            <li>Developer - apply here <a href="https://acmurl.com/ai-dev">https://acmurl.com/ai-dev</a></li>
-            <li>Event Lead - apply here <a href="https://acmurl.com/ai-events">https://acmurl.com/ai-events</a></li>
-            <li>Marketing / Sponsorship - apply here <a href="https://acmurl.com/ai-marketing-sponsorship-app">https://acmurl.com/ai-marketing-sponsorship-app</a></li>
-            <p>More details, including application forms, can be found on our discord</p>
+            <p>
+              We are always looking for more people to join our mission and
+              build the growing AI community at UCSD. The following roles are
+              open to all undergraduate UCSD students:
+            </p>
+            <li>
+              Developer - apply here{' '}
+              <a href="https://acmurl.com/ai-dev">https://acmurl.com/ai-dev</a>
+            </li>
+            <li>
+              Event Lead - apply here{' '}
+              <a href="https://acmurl.com/ai-events">
+                https://acmurl.com/ai-events
+              </a>
+            </li>
+            <li>
+              Marketing / Sponsorship - apply here{' '}
+              <a href="https://acmurl.com/ai-marketing-sponsorship-app">
+                https://acmurl.com/ai-marketing-sponsorship-app
+              </a>
+            </li>
+            <p>
+              More details, including application forms, can be found on our
+              discord
+            </p>
           </div>
-          <div className='main-section' id='events'>
-            <h2 className='statement' id='learn-ai'>Upcoming Events!</h2>
-            <p>These are our upcoming events on workshops, competitions, networking and more!</p>
-            <Row gutter={[24, 24]} justify='center'>
+          <div className="main-section" id="events">
+            <h2 className="statement" id="learn-ai">
+              Upcoming Events!
+            </h2>
+            <p>
+              These are our upcoming events on workshops, competitions,
+              networking and more!
+            </p>
+            <Row gutter={[24, 24]} justify="center">
               {eventData.map((event) => {
-                return <Col><EventCard event={event}/></Col>
+                return (
+                  <Col>
+                    <EventCard event={event} />
+                  </Col>
+                );
               })}
             </Row>
           </div>
@@ -56,4 +113,4 @@ function MainPage() {
   );
 }
 
-export default MainPage
+export default MainPage;
