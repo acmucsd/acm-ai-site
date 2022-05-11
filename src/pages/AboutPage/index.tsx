fe72f4469ec3b9c91fa8783555b552f359eb6066
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-05-10 19:27:25 -0700

    refactor: clean up about page

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './index.less';
 import DefaultLayout from '../../components/layouts/default';
 import { Col, Row } from 'antd';
 import { Card } from '../../components/Card';
-import { directors, operations, dev, external} from './text';
+import { directors, operations, dev, external, Person} from './people';
 
 const ColoredLine = () => (
   <hr
@@ -15,156 +15,76 @@ const ColoredLine = () => (
   />
 );
 
+const CardsRow = ({ children }: { children?: ReactNode }) => (
+  <Row
+    gutter={[
+      { xs: 8, sm: 16, md: 24, lg: 32 },
+      { xs: 8, sm: 16, md: 24, lg: 32 },
+    ]}
+    justify="center"
+  >
+    {children}
+  </Row>
+)
+
+const AboutCard = ({ card }: { card: Person }) => (
+  <Col xs={12} md={8}>
+    <Card cover={!card.picture ? null : (
+      <img
+        src={card.picture}
+        alt={undefined}
+      />
+    )}
+    >
+      <h3>{`${card.role}: ${card.name}`}</h3>
+      <p className="position">
+        {`${card.year} ${card.major} major`}
+      </p>
+      <ColoredLine />
+      <p className="quote">{card.bio}</p>
+    </Card>
+  </Col>
+);
+
+const renderCards = (people: Person[]) => (
+  <>{people.map((card) => <AboutCard card={card} key={card.name} />)}</>
+);
+
+const Section = ({ statement, people }: { statement: string, people: Person[] }) => (
+  <div className="main-section">
+    <h2 className="statement">{statement}</h2>
+    <br />
+    <CardsRow>
+      {renderCards(people)}
+    </CardsRow>
+  </div>
+)
+
 const AboutPage = () => {
   return (
-
     <DefaultLayout>
       <div className="AboutPage">
         <div className="hero">
           <h1 id="title">Hello, we are ACM AI at UCSD</h1>
         </div>
         <div>
-        <div className="main-section">
-        <h2 className="statement">And these are the people running the show!</h2>
-        <br />
-        <Row
-          gutter={[
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-          ]}
-          justify="center"
-        >
-          {directors.map((cardText) => {
-            return (
-              <Col xs={12} md={8}>
-                <Card cover={<img
-                        src={cardText.picture}
-                        alt="Nope"
-                        width="40%"
-                        height="40%"
-                      ></img>}
-                >
-                  <h3>{cardText.role +': '}{cardText.name}</h3>
-                  <p className="position">
-                    {cardText.year + ' '}
-                    {cardText.major + ' major'}
-                  </p>
-                  <ColoredLine />
-                  <p className="quote">
-                    {cardText.bio}
-                  </p>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
+          <Section
+            people={directors}
+            statement="And these are the people running the show!"
+          />
+          <Section
+            people={operations}
+            statement="Our team of event leads who design and host all the events"
+          />
+          <Section
+            people={dev}
+            statement="Our team of developers working tirelessly to keep everything running smoothly"
+          />
+          <Section
+            people={external}
+            statement="Our wonderful marketing and sponsorship team"
+          />
         </div>
-        <div className="main-section">
-          <h2 className="statement"> Our team of event leads who design and host all the events </h2>
-        <br />
-        <Row
-          gutter={[
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-          ]}
-          justify="center"
-        >
-          {operations.map((cardText) => {
-            return (
-              <Col xs={12} md={8}>
-                <Card cover={<img
-                        src={cardText.picture}
-                        alt="Nope"
-                        width="40%"
-                        height="40%"
-                      ></img>}
-                >
-                <h3>{cardText.role +': '}{cardText.name}</h3>
-                  <p className="position">
-                    {cardText.year + ' '}
-                    {cardText.major + ' major'}
-                  </p>
-                  <ColoredLine />
-                  <p className="quote">
-                    {cardText.bio}
-                  </p>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-        </div>
-        <div className="main-section">
-           <h2 className="statement"> Our team of developers working tirelessly to keep everything running smoothly</h2>
-        <br />
-        <Row
-          gutter={[
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-          ]}
-          justify="center"
-        >
-          {dev.map((cardText) => {
-            return (
-              <Col xs={12} md={8}>
-                <Card   cover={<img
-                        src={cardText.picture}
-                        alt="Nope"
-                        width="40%"
-                        height="40%"
-                      ></img>}
-                >
-                  <h3>{cardText.role +': '}{cardText.name}</h3>
-                  <p className="position">
-                    {cardText.year + ' '}
-                    {cardText.major + ' major'}
-                  </p>
-                  <ColoredLine />
-                  <p className="quote">
-                    {cardText.bio}
-                  </p>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-        </div>
-        <div className="main-section">
-          <h2 className="statement"> Our wonderful marketing and sponsorship team </h2>
-        <br />
-        <Row
-          gutter={[
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-          ]}
-          justify="center"
-        >
-          {external.map((cardText) => {
-            return (
-              <Col xs={12} md={8}>
-                <Card cover={<img
-                        src={cardText.picture}
-                        alt="Nope"
-                        width="40%"
-                        height="40%"
-                      ></img>}
-                >
-                  <h3>{cardText.role +': '}{cardText.name}</h3>
-                  <p className="position">
-                    {cardText.year + ' '}
-                    {cardText.major + ' major'}
-                  </p>
-                  <ColoredLine />
-                  <p className="quote">
-                    {cardText.bio}
-                  </p>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-        </div>
-      </div>
       </div>
     </DefaultLayout>
   );
