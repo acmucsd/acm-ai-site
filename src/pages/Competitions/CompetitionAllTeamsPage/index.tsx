56ccaa9bb1751d229cf63d0c1a5aa0b3df9a23b5
jenetic <jznnylam@gmail.com>
2023-01-17 22:33:55 -0800

    Made new page to display all teams, currently console.logs all teams of a competition

--- src/pages/Competitions/CompetitionAllTeamsPage/index.tsx
+++ src/pages/Competitions/CompetitionAllTeamsPage/index.tsx
@@ -4,8 +4,40 @@ import Card from '../../../components/Card';
 import { Form, Input, message, Button } from 'antd';
 import './index.less';
 import { Table, Modal } from 'antd';
+import axios, { AxiosResponse } from 'axios';
+import { COOKIE_NAME } from '../../../configs';
+import { getToken } from '../../../utils/token';
+
+export const getTeams = async (
+  competitionName: string
+): Promise<AxiosResponse> => {
+  let token = getToken(COOKIE_NAME);
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        // TODO: remove hardcoded thing
+        `http://localhost:9000/v1/competitions/teams/${competitionName}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      )
+      .then((res: AxiosResponse) => {
+        console.log(res)
+        // resolve(res);
+      })
+      .catch((error) => {
+        message.error(error.response.data.error.message);
+        reject(error);
+      });
+  });
+};
 
 const CompetitionAllTeamsPage = () => {
+
+  getTeams('TestCompetition1');
 
   return (
     <DefaultLayout>
