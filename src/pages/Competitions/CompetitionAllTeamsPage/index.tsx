2b837dfaea514690cda5335757a26c0d8758eefc
jenetic <jznnylam@gmail.com>
2023-01-18 00:20:10 -0800

    Retrieve specific team data from API and display it

--- src/pages/Competitions/CompetitionAllTeamsPage/index.tsx
+++ src/pages/Competitions/CompetitionAllTeamsPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import DefaultLayout from '../../../components/layouts/default';
 import { message } from 'antd';
 import './index.less';
