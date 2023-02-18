001d4a7e42351409541ee4c8f42d5e03e50cbdfc
stone_tao <stonezt2019@gmail.com>
2023-02-17 18:42:10 -0800

    updates

--- src/pages/Competitions/CompetitionTeamPages/SubmissionDetailsPage/index.tsx
+++ src/pages/Competitions/CompetitionTeamPages/SubmissionDetailsPage/index.tsx
@@ -2,9 +2,32 @@ import React, { useEffect, useState } from 'react';
 import DefaultLayout from '../../../../components/layouts/default';
 import './index.less';
 import { Link, useParams } from 'react-router-dom';
-import { getSubmissionDetails } from '../../../../actions/teams/utils';
-import { Button } from 'antd';
+import { getSubmissionDetails, getTeamInfo } from '../../../../actions/teams/utils';
+import { Button, Table } from 'antd';
 import BackLink from '../../../../components/BackLink';
+import { getSubmissionMatches, getSubmissionReplay } from '../../../../actions/competition';
+import { ColumnsType } from 'antd/lib/table';
+const columns: ColumnsType<MatchData> = [
+  {
+    title: 'ID',
+    dataIndex: '_id',
+  },
+  {
+    title: "Teams",
+    render(value, record, index) {
+      return value.competitionTeams.map((v: any) => v.teamName).join(", ");
+    },
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date'
+  },
+];
+interface MatchData {
+  competitionTeams: string[];
+  competitionEntries: string[];
+  _id: string;
+}
 
 const CompetitionSubmissionDetailsPage = () => {
   let { competitionName, submissionId, teamName } = useParams<{
@@ -13,13 +36,25 @@ const CompetitionSubmissionDetailsPage = () => {
     submissionId: string;
   }>();
 
-  const [submissionDetails, setSubmissionDetails] = useState<any>(null);
 
+  const [submissionDetails, setSubmissionDetails] = useState<any>(null);
+  const [submissionMatches, setSubmissionMatches] = useState<any>(null);
+  const [teams, setTeams] = useState<Record<string, string>>({});
   useEffect(() => {
     getSubmissionDetails(competitionName, submissionId).then((res) => {
       setSubmissionDetails(res.data);
     });
+    getSubmissionMatches(competitionName, submissionId).then((res) => {
+      console.log("Retrieved Match Data");
+      setSubmissionMatches(res.data);
+      // getSubmissionReplay(competitionName, res.data[0]._id).then((res) => {
+      // });
+    });
   }, []);
+
+  if (!submissionMatches || !submissionDetails) {
+    return <DefaultLayout></DefaultLayout>
+  }
 
   return (
     <DefaultLayout>
@@ -30,6 +65,9 @@ const CompetitionSubmissionDetailsPage = () => {
         </Link>
         <h2>Submission {submissionId}</h2>
         <div>{JSON.stringify(submissionDetails)}</div>
+        <div>
+        <Table columns={columns} dataSource={submissionMatches} />
+        </div>
       </div>
     </DefaultLayout>
   );
