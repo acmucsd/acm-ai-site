cf2a272b6cb6ec30d065eabfa42f035776d334ac
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2023-08-30 22:29:45 -0700

    Update events page with antd redesign and timeline

--- src/pages/EventsPage/index.tsx
+++ src/pages/EventsPage/index.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './index.less';
 import DefaultLayout from '../../components/layouts/default';
 import { ACMEvent, fetchFutureEvents } from '../../actions/events';
-import EventCard from '../../components/EventCard';
+import EventTimeline from '../../components/EventTimeline';
+import { Empty, Row, Col, Layout} from 'antd';
+const { Content, Footer } = Layout;
 
 function EventsPage(props: any) {
   const [eventData, setEventData] = useState<Array<ACMEvent>>([]);
@@ -10,44 +12,51 @@ function EventsPage(props: any) {
   useEffect(() => {
     fetchFutureEvents().then((data) => {
       setEventData(data);
+      console.log(data);
     });
   }, []);
-  const haveFutureEvents = eventData.length < 0;
+
   return (
     <DefaultLayout>
-      <div className = "EventsPage">
-        <div className = "hero">
-          <h1 id = "title">Events</h1>
-          <p className = "subtext">
-            Welcome to our events! Learn about what events we run and how to find them.
-            <div className="upcomingEvents">
-                {haveFutureEvents && <>These are the upcoming events <br/></>}
-                <div className="eventsBlock">
-                {
-                haveFutureEvents ? <EventCard event={eventData[0]} />  : "There are no upcoming events at this time."
-              }
-              </div>
+      <div className="Events">
+        <Content className="eventsHeader">
+            <h1 className="eventsTitle">Upcoming Events</h1>
+            <h3 className="eventsSubTitle">make friends • have fun • learn something new</h3>
+
+          <div className="upcomingEvents">
+            <EventTimeline eventData={eventData}/>
           </div>
-          </p>
-        </div>
-        
-        <div>
-        <div className = "main-section">
-            <h1 className = "statement">What events do we run?</h1>
-            <p className = "subtext">
-              We run all kinds of events, from intro to deep learning to seminars from distinguished researchers and professors. Events are a great way to be engaged with the AI community at UCSD as well as a way to learn content you may not typically learn in class! 
-              <br />
-              We also have a running contest where we give out kahoot rankings on discord based on your ranking on our latest kahoot quizzes. We often host these quizzes at the start or end of a workshop.
-            </p>
-         </div>
-          <div className = "main-section">
-            <h1 className = "statement">Where to find our workshops?</h1>
-            <p className = "subtext">
-              We post all our workshop recordings on our youtube at <a href='https://acmurl.com/youtube' target='_blank' rel="noopener noreferrer">https://acmurl.com/youtube</a>. Stay tuned in our Discord for when we upload them.
-            </p>
+        </Content>
+
+        <Content>
+          <div className="gradientBar">
           </div>
-        </div>
+        </Content>
+        <Footer className="eventsFooter">
+          <Row className="row">
+            <Col className="infoText">
+              <h2>What events do we run?</h2>
+              <p>We run all kinds of events, from intro to deep learning workshops to seminars from distinguished researchers and professors. 
+                Events are a great way to engage with the AI community at UCSD and learn content you may not 
+                typically learn in class! We also have a running contest ranking on Discord based on your performance on 
+                our latest Kahoot quizzes. We often host these quizzes at the start or end of a workshop.
+              </p>
+            </Col>
+          </Row>
+
+          <Row className="row">
+            <Col className="infoText">
+              <h2>Where can I find past workshops?</h2>
+              <p>We post all of our workshop recordings on our YouTube at <a href='https://acmurl.com/youtube' target='_blank' rel="noopener noreferrer">https://acmurl.com/youtube</a>. 
+              Stay tuned in our Discord for uploads!
+              </p>
+            </Col>
+          </Row>
+
+          <h3>ACM AI at UCSD 2023</h3>
+        </Footer>
       </div>
+
     </DefaultLayout>
   );
 }
