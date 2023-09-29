f591aaa73cbf15c9f54dd858a675f4a26bfad181
vgdc <vgdc@ucsd.edu>
2023-09-29 16:18:39 -0700

    tweaked page margins

--- src/components/layouts/default.tsx
+++ src/components/layouts/default.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../Header';
 import Container from './container';
+import '../../newStyles/index.less';
 
 function DefaultLayout(props: any) {
   return (
     <div>
       <Header></Header>
-      <Container>{props.children}</Container>
+      <Container style = {{width: "100%"}}>{props.children}</Container>
     </div>
   );
 }
