7f14cd6b909a0420366ca310116454d5486dc17e
William Kim <wik002@sdsc.edu>
2023-12-10 22:24:50 -0800

    experimented with fetching competition data

--- src/pages/Competitions/CompetitionPortalPage/index.tsx
+++ src/pages/Competitions/CompetitionPortalPage/index.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from "react";
-import { List, Tabs, message } from "antd";
+import React, { useContext, useEffect, useState } from "react";
+import { Avatar, List, Tabs, message } from "antd";
 import { Layout, Space, Button } from 'antd';
 import UserContext from "../../../UserContext";
 import { useHistory } from 'react-router-dom';
@@ -7,21 +7,49 @@ import {
     getTeamInfo,
     createTeam,
     getCompetitionUser,
+    getTeams,
   } from '../../../actions/teams/utils';
 
 import './index.less';
 import path from 'path';
 
 import DefaultLayout from "../../../components/layouts/default";
+import { PaginationPosition, PaginationAlign } from "antd/es/pagination/Pagination";
+import { all } from "axios";
 
 const { Content } = Layout;
 
 
+const teamCard = (team: any): React.ReactNode => {
+    return (
+        <div id = {team.teamID} className = "teamPreviewCard">
+            <h3>{team.teamName}</h3>
+            <p>{team.teamMembers.length} members</p>
+            {/* {team.teamMembers.map((member: string, index: number) => (
+                <p key={index}>{member}</p>
+            ))} */}
+        </div>
+    );
+};
 
-const FindTeamsTab = (): React.ReactNode => {
+
+const FindTeamsTab = (data: Array<Object>): React.ReactNode => {
+    const [position, setPosition] = useState<PaginationPosition>('bottom');
+    const [align, setAlign] = useState<PaginationAlign>('center');
+  
     return (
       <Content id="findTeamsContainer" className = "portalTabContent">
-        <h2>Find Teams</h2>
+        <List
+            split = {false}
+            pagination={{ position, align}}
+            dataSource={data}
+            renderItem={(item: any) => (
+            <List.Item key = {item.competitionName}>
+                {teamCard(item)}
+
+            </List.Item>
+            )}
+        />
 
       </Content>
     );
@@ -35,7 +63,7 @@ const LeaderBoardTab = (): React.ReactNode => {
 
       </Content>
     );
-  };
+};
 
 
   const MyTeamTab = (): React.ReactNode => {
@@ -51,6 +79,8 @@ const LeaderBoardTab = (): React.ReactNode => {
 function CompetitionPortalPage ()  {
     const history = useHistory();
     const { user } = useContext(UserContext);
+    const [allTeams, setAllTeams] = useState<Array<Object>>([]);
+
 
     const tabItems= [
         {
@@ -61,29 +91,36 @@ function CompetitionPortalPage ()  {
         {
             label: <p>Find Teams</p>,
             key: '2',
-            children: FindTeamsTab(),
+            children: FindTeamsTab(allTeams),
         },
         {
             label: <p>My Team</p>,
             key: '3',
             children: MyTeamTab(),
         }
-        ];
+    ];
 
     useEffect(() => {
         if (!user.loggedIn) {
           message.info('You need to login to upload predictions and participate');
           history.replace(path.join(window.location.pathname, '../../../login'));
         }
-      }, []);
 
-
-      useEffect(() => {
-        if (!user.loggedIn) {
-          message.info('You need to login to upload predictions and participate');
-          history.replace(path.join(window.location.pathname, '../../../login'));
+        else {
+            
+            getTeams("TestCompetition2").then(res => {
+                if(res.data) {
+                    setAllTeams(Array.from(res.data))
+                    console.log(Array.from(res.data));
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
         }
+        
       }, []);
+
     
     return (
         <DefaultLayout>
@@ -101,18 +138,32 @@ function CompetitionPortalPage ()  {
                     </section>
                 </Content>
 
-                
-
-    
             
-
                 <Content id = "portalTabContent">
                     {/* If the user is not part of a team yet, then */}
                     <Tabs
                         size="small"
                         animated={true}
                         tabPosition="top"
-                        items={tabItems}
+                        items={
+                            [
+                                {
+                                    label: <p>Leaderboard</p>,
+                                    key: '1',
+                                    children: LeaderBoardTab(),
+                                },
+                                {
+                                    label: <p>Find Teams</p>,
+                                    key: '2',
+                                    children: FindTeamsTab(allTeams),
+                                },
+                                {
+                                    label: <p>My Team</p>,
+                                    key: '3',
+                                    children: MyTeamTab(),
+                                }
+                            ]
+                        }
                     ></Tabs>
                 </Content>
 
