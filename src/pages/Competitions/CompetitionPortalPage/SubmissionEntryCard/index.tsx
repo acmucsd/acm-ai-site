91f645ff140bd0a5456c06fccce6386e2463934e
William Kim <wik002@sdsc.edu>
2024-01-04 16:32:59 -0800

    added folders for submission entry card

---
+++ src/pages/Competitions/CompetitionPortalPage/SubmissionEntryCard/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const SubmissionEntryCard = (data: any) => {
+
+    
+    return (
+        <div>
+            <span id = "submissionCardHeader">
+                
+            </span>
+            <h3 id = "submissionUser"></h3>
+            <p id ="submissionDescription"></p>
+            <span id = "submissionFileRow"></span>
+        </div>
+    );
+}
+
+export default SubmissionEntryCard;
