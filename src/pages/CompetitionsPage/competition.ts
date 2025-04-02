f8457df7b6f97fa60ab9305e0a7476446e89adcf
Samantha Prestrelski <33042752+sprestrelski@users.noreply.github.com>
2025-04-01 23:16:33 -0700

    Add 2025 competition (#159)
    * feat: add starchess.ai
    
    * chore: remove old landing pages
    
    * feat: basic new css styling
    
    * feat: add starchess logo

---
+++ src/pages/CompetitionsPage/competition.ts
@@ -0,0 +1,15 @@
+export interface Competition {
+    competitionName: string;
+    icon: string;
+    year: string;
+    stats: {
+        "In Prizes"?: number;
+        Signups?: number;
+        Teams?: number;
+        Submissions?: number;
+        Matches?: number;
+    };
+    description: string;
+    link?: string;
+    leaderboard?: string;
+}
