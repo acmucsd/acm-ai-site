fd627f353b48ec4a2e6584f5c9476679a553ac81
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2025-04-19 07:10:31 -0700

    change back to ai

--- src/pages/Competitions/CompetitionPortalPage/index.tsx
+++ src/pages/Competitions/CompetitionPortalPage/index.tsx
@@ -577,7 +577,7 @@ const MyTeamTab = ( { isLoadingTeamInfo, compUser, rankData, teamInfo, metaData 
 function CompetitionPortalPage() {
 
     // This enables us to specify the most current competition
-    const competitionName = "StarChess.TEST";
+    const competitionName = "StarChess.AI";
     const history = useHistory();
 
     // User profile data
